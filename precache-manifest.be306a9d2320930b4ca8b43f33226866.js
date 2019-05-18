fe4c2a8b20f397b62aef4fc1fe3f7ae1584f279a
Will Neto <wiltonribeiro@alu.ufc.br>
2019-05-18 14:50:42 -0300

    Updates

---
+++ precache-manifest.be306a9d2320930b4ca8b43f33226866.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "602e8477a65baf834b0aa2892a2c659e",
+    "url": "/bloc-react/index.html"
+  },
+  {
+    "revision": "7b754299c6b386b9123c",
+    "url": "/bloc-react/static/css/main.64f86b71.chunk.css"
+  },
+  {
+    "revision": "8a86505582106bb62329",
+    "url": "/bloc-react/static/js/2.68ad71db.chunk.js"
+  },
+  {
+    "revision": "7b754299c6b386b9123c",
+    "url": "/bloc-react/static/js/main.4d95a83f.chunk.js"
+  },
+  {
+    "revision": "8a332c2ab9addae55a34",
+    "url": "/bloc-react/static/js/runtime~main.042e3477.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bloc-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
